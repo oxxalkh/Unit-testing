26e55a28bf3b84d90f40b81a6863bcab756165db
ksmos <ksmosc@mail.ru>
2024-02-13 12:17:16 +0300

    first

---
+++ unit-tests-lesson02/unit-tests-lesson02/Test/seminars/second/model/ProductTest.java
@@ -0,0 +1,24 @@
+package seminars.second.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void getQuantity() {
+    }
+}
